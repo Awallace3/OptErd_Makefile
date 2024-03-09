7874a2609e320f183b0fed68f861d15cf60b4870
Austin Wallace <awallace43@gatech.edu>
2024-03-09 17:54:38 -0500

    updated cmake configs

--- external/share/cmake/erd/erdConfig.cmake
+++ external/share/cmake/erd/erdConfig.cmake
@@ -4,11 +4,11 @@ project(erd LANGUAGES C CXX Fortran)
 # include("${CMAKE_CURRENT_LIST_DIR}/CIntTargets.cmake")
 
 # Set the include directory
-set(CInt_INCLUDE_DIR "${CMAKE_CURRENT_LIST_DIR}/../../../include")
+set(ERD_INCLUDE_DIR "${CMAKE_CURRENT_LIST_DIR}/../../../include")
 
 # Set the library path
-set(CInt_LIBRARY "${CMAKE_CURRENT_LIST_DIR}/../../../lib/liberd.a")
+set(ERD_LIBRARY "${CMAKE_CURRENT_LIST_DIR}/../../../lib/liberd.a")
 
 # You can set this if you want to make it easy to link to the library
-set(CInt_LIBRARIES "${ERD_LIBRARY}")
-set(CInt_INCLUDE_DIRS "${ERD_INCLUDE_DIR}")
+set(ERD_LIBRARIES "${ERD_LIBRARY}")
+set(ERD_INCLUDE_DIRS "${ERD_INCLUDE_DIR}")
--- external/share/cmake/oed/oedConfig.cmake
+++ external/share/cmake/oed/oedConfig.cmake
@@ -1,14 +1,14 @@
 cmake_minimum_required(VERSION 3.5)
 project(oed LANGUAGES C CXX Fortran)
 
-# include("${CMAKE_CURRENT_LIST_DIR}/CIntTargets.cmake")
+# include("${CMAKE_CURRENT_LIST_DIR}/OEDTargets.cmake")
 
 # Set the include directory
-set(CInt_INCLUDE_DIR "${CMAKE_CURRENT_LIST_DIR}/../../../include")
+set(OED_INCLUDE_DIR "${CMAKE_CURRENT_LIST_DIR}/../../../include")
 
 # Set the library path
-set(CInt_LIBRARY "${CMAKE_CURRENT_LIST_DIR}/../../../lib/liboed.a")
+set(OED_LIBRARY "${CMAKE_CURRENT_LIST_DIR}/../../../lib/liboed.a")
 
 # You can set this if you want to make it easy to link to the library
-set(CInt_LIBRARIES "${OED_LIBRARY}")
-set(CInt_INCLUDE_DIRS "${OED_INCLUDE_DIR}")
+set(OED_LIBRARIES "${OED_LIBRARY}")
+set(OED_INCLUDE_DIRS "${OED_INCLUDE_DIR}")
